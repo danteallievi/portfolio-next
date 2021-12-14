44e2e186d1d963b2207a40018e18d8dc6940938f
Dante Allievi <danteallievi@gmail.com>
2021-12-14 21:16:56 +0100

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.scss";
+
+const Home: NextPage = () => {
+  return <h1 className={styles.heading}>Hi</h1>;
+};
+
+export default Home;
