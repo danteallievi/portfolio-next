0d392e3c8018cfaecf5c964f98aaf9090b5163c2
Dante Allievi <danteallievi@gmail.com>
2021-12-19 18:54:25 +0100

    Side bar component created

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,53 @@
+import styles from "../../styles/components/Sidebar.module.scss";
+import {
+  faAddressBook,
+  faEnvelope,
+  faMapMarkerAlt,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SocialIcons from "../SocialIcons/SocialIcons";
+
+const Sidebar = () => {
+  return (
+    <aside className={styles.sideCard}>
+      <div className={styles.sideCard__title}>
+        <h1 className={styles.sideCard__title__mainTitle}>
+          <span>D</span>ante <span>A</span>llievi
+        </h1>
+        <p className={styles.sideCard__title__subTitle}>Web Developer</p>
+      </div>
+
+      <SocialIcons />
+
+      <div className={styles.sideCard__resume}>
+        <a
+          href="/assets/DANTE-ALLIEVI-EN.pdf"
+          download="Dante Allievi Resume.pdf"
+          className={styles.sideCard__resume__downloadLink}
+        >
+          <FontAwesomeIcon icon={faAddressBook} />
+          <span>Download Resume</span>
+        </a>
+      </div>
+
+      <div className={styles.sideCard__data}>
+        <div>
+          <FontAwesomeIcon icon={faMapMarkerAlt} />
+          <p>Barcelona, Spain</p>
+        </div>
+        <div className={styles.sideCard__data__mailSpan}>
+          <FontAwesomeIcon icon={faEnvelope} />
+          <p>danteallievi@gmail.com</p>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faPhone} />
+          <p>+34 1231543</p>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
