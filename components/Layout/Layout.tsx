77b6da90d701d9d46a071c9f1a6cfc84e4d0e2b6
Dante Allievi <danteallievi@gmail.com>
2021-12-19 18:47:23 +0100

    Moved basic layout to the Layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,21 @@
+import styles from "../../styles/components/Layout.module.scss";
+import Sidebar from "../Sidebar/Sidebar";
+
 interface ILayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: ILayoutProps) => {
-  return <main className="main-content">{children}</main>;
+  return (
+    <>
+      <main className={styles.mainContent}>
+        <Sidebar />
+        <section className={styles.mainContent__articleContent}>
+          {children}
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Layout;
