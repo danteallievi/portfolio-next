bbc0d21bfecdc276104e93e2868a5b351bea74ef
Dante Allievi <danteallievi@gmail.com>
2021-12-23 14:50:21 +0100

    Modified theme button to use the new package

--- components/ThemeButton/ThemeButton.tsx
+++ components/ThemeButton/ThemeButton.tsx
@@ -1,51 +1,32 @@
-import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import styles from "../../styles/components/ThemeButton.module.scss";
 
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ThemeButton = () => {
-  const [darkTheme, setDarkTheme] = useState(false);
-
-  const checkDarkTheme = () => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-      localStorage.theme = "dark";
-      setDarkTheme(true);
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.theme = "light";
-      setDarkTheme(false);
-    }
-  };
-
-  useEffect(() => {
-    checkDarkTheme();
-  }, []);
+  const { theme, setTheme } = useTheme();
 
   const handleToggleTheme = () => {
-    if (darkTheme) {
-      setDarkTheme(false);
-      localStorage.theme = "light";
-      document.documentElement.classList.remove("dark");
-      console.log(document.documentElement.classList);
+    if (theme === "dark") {
+      setTheme("light");
     } else {
-      setDarkTheme(true);
-      localStorage.theme = "dark";
-      document.documentElement.classList.add("dark");
-      console.log(document.documentElement.classList);
+      setTheme("dark");
     }
   };
+
+  // const IconToRender = () => {
+  //   if (theme === "dark") {
+  //     return;
+  //   } else {
+  //     return <FontAwesomeIcon icon={faMoon} />;
+  //   }
+  // };
 
   return (
     <>
       <button className={styles.themeButton} onClick={handleToggleTheme}>
-        <FontAwesomeIcon icon={darkTheme ? faSun : faMoon} />
-        {darkTheme ? "Light" : "Dark"} mode
+        Sharau nekito
       </button>
     </>
   );
