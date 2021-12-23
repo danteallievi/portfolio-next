2845e173392185f952154727da6e023c58a7a514
Dante Allievi <danteallievi@gmail.com>
2021-12-23 20:54:15 +0100

    Added head titlte to the pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 // import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Dante Allievi Portfolio</title>
+      </Head>
       <h1>hi</h1>
     </>
   );
