fdb459b8b597dbf59b649684b5258078b4261964
Dante Allievi <danteallievi@gmail.com>
2022-02-14 20:45:07 +0100

    ProjectCard component created

---
+++ components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,11 @@
+import styles from "./ProjectCard.module.scss";
+
+const ProjectCard = () => {
+  return (
+    <>
+      <h1 className={styles.hola}>Hi</h1>
+    </>
+  );
+};
+
+export default ProjectCard;
