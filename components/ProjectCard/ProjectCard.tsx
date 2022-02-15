0e8226b2690aa33320adc1dc56176999600205a9
Dante Allievi <danteallievi@gmail.com>
2022-02-15 15:37:45 +0100

    ProjectCard styles and component createds

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
+import { IProjectCard } from "../../interfaces/interfaces";
 import styles from "./ProjectCard.module.scss";
 
-const ProjectCard = () => {
+const ProjectCard = ({ name, imagePath, onClick }: IProjectCard) => {
   return (
-    <>
-      <h1 className={styles.hola}>Hi</h1>
-    </>
+    <div className={styles.card}>
+      <Image
+        className={styles.card__image}
+        src={imagePath}
+        alt={name}
+        onClick={onClick}
+        width={350}
+        height={200}
+      />
+      <h2 className={styles.card__title}>{name}</h2>
+    </div>
   );
 };
 
